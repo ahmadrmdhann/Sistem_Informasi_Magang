bd3eaa4f20932826e95217f38a839be3282b149b
ahmadrmdhann <ahmadramadhansqorpio04@gmail.com>
2025-05-14 16:58:10 +0700

    feat: integrate Tailwind CSS and Preline into Laravel project
    - Added Tailwind CSS and Preline dependencies in package.json
    - Created tailwind.config.js for Tailwind CSS configuration
    - Updated app.css to include Tailwind CSS and Preline styles
    - Removed default welcome.blade.php view to customize layout
    - Updated vite.config.js to include Tailwind CSS plugin

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    purge: [],
+    darkMode: false,
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js"
+    ],
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
