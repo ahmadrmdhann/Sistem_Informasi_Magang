e6e2086bf829a3f50bc0a4dd97368de51362051b
AdriFavian <adrifavian2@gmail.com>
2025-06-08 06:13:20 +0700

    feat: remove unused notification

--- resources/views/components/notification.blade.php
+++
@@ -1,7 +0,0 @@
-{{-- 
-    Komponen Notifikasi Standar Blade (Bukan Lagi Komponen Terpisah)
-    File ini tidak lagi digunakan karena notifikasi diimplementasikan langsung di view masing-masing.
-    Anda bisa menghapus file ini jika tidak ada referensi lain.
---}}
-
-{{-- Konten lama sengaja dikosongkan/dihapus karena tidak lagi relevan --}}
